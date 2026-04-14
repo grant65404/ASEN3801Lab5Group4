f2e994c357f31b3e42ada564acf981bd102f805f
j3sselarson <jela7710@colorado.edu>
2026-04-14 14:01:48 -0600

    Final for Problem 2 from Jesse
    Gifts!

--- PlotAircraftSim.m
+++ PlotAircraftSim.m
@@ -1,168 +1,140 @@
-function PlotAircraftSim(time, aircraft_state_array, control_input_array,fig, col, bool, leg)
-% if you want to save the plots, set bool to 1. default is no save. leg
-% produces generalized legends for like multiple plot cases
-arguments
-        time;
-        aircraft_state_array;
-        control_input_array;
-        fig = 1:1:6; % optional
-        col = 'b'; % optional 
-        bool = 0;   % optional
-        leg = ""; % optional
+function PlotAircraftSim(time, aircraft_state_array, control_input_array, fig, col)
+    
+    % Determine case name from figure numbers to plot things neatly
+    if fig(1) == 1
+        casename = 'Case 2.1 (Only Forward Velocity)';
+        elseif fig(1) == 7
+            casename = 'Case 2.2: SLUF Flight';
+        elseif fig(1) == 13
+            casename = 'Case 2.3: Spiral Mode';
     end
-set(groot,'defaultAxesXGrid','on') % setting grids on for all plots
-set(groot,'defaultAxesYGrid','on')
-set(groot,'defaultAxesZGrid','on')
 
-figure(fig(1)); % the first figure containing inertial position
-sgtitle("Intertial Position") % large title for subplots
-subplot(3,1,1);
-plot(time, aircraft_state_array(1,:), col, linewidth=1.3); hold on;
-title("x position");
-xlabel("time (s)");
-ylabel("dist. (m)");
-subplot(3,1,2);
-plot(time, aircraft_state_array(2,:), col, linewidth=1.3); hold on;
-title("y position")
-xlabel("time (s)");
-ylabel("dist. (m)");
-subplot(3,1,3);
-plot(time,aircraft_state_array(3,:),col, linewidth=1.3); hold on; % negative z 
-title("-z position")
-xlabel("time (s)");
-ylabel("dist. (m)");
-if leg ~= "" % if the user inputs a legend name it places it in an ideal spot
-    legend(leg,'Position',[0.633841768165843 0.899580306092237 0.205357139451163 0.0773809503941308])
-end
-if bool==true % if user wants to save plots, plots save
-    f = gcf;
-    f.WindowState = 'maximized'; % fullscreen plot
-    pause(0.5); 
-    exportgraphics(f, "figure"+ num2str(fig(1)) +".png", 'Resolution', 300);
-end
-figure(fig(2));
-sgtitle("Euler Angles")
-subplot(3,1,1);
-plot(time, aircraft_state_array(4,:), col, linewidth=1.3); hold on;
-title("Roll Angle")
-xlabel("time (s)");
-ylabel("\phi (rad)");
-subplot(3,1,2);
-plot(time, aircraft_state_array(5,:), col, linewidth=1.3); hold on;
-title("Pitch Angle")
-xlabel("time (s)");
-ylabel("\theta (rad)");
-subplot(3,1,3);
-plot(time,aircraft_state_array(6,:),col, linewidth=1.3); hold on;
-title("Yaw Angle")
-xlabel("time (s)");
-ylabel("\psi (rad)");
-if leg ~= ""
-    legend(leg,'Position',[0.633841768165843 0.899580306092237 0.205357139451163 0.0773809503941308])
-end
-if bool==true
-    f = gcf;
-    f.WindowState = 'maximized'; 
-    pause(0.5); 
-    exportgraphics(f, "figure"+num2str(fig(2))+".png", 'Resolution', 300);
-end
-figure(fig(3));
-sgtitle("Intertial Velocity in Body Frame")
-subplot(3,1,1);
-plot(time, aircraft_state_array(7,:), col, linewidth=1.3); hold on;
-title("x velocity")
-xlabel("time (s)");
-ylabel("u (m/s)");
-subplot(3,1,2);
-plot(time, aircraft_state_array(8,:), col, linewidth=1.3); hold on;
-title("y velocity")
-xlabel("time (s)");
-ylabel("v (m/s)");
-subplot(3,1,3);
-plot(time,-aircraft_state_array(9,:),col, linewidth=1.3); hold on;
-title("-z velocity")
-xlabel("time (s)");
-ylabel("w (m/s)");
-if leg ~= ""
-    legend(leg,'Position',[0.633841768165843 0.899580306092237 0.205357139451163 0.0773809503941308])
-end
-if bool==true
-    f = gcf;
-    f.WindowState = 'maximized'; 
-    pause(0.5); 
-    exportgraphics(f, "figure"+num2str(fig(3))+".png", 'Resolution', 300);
-end
-figure(fig(4))
-sgtitle("Angular Velocity")
-subplot(3,1,1);
-plot(time, aircraft_state_array(10,:), col, linewidth=1.3); hold on;
-title("Roll Rate")
-xlabel("time (s)");
-ylabel("p (rad/s)");
-subplot(3,1,2);
-plot(time, aircraft_state_array(11,:), col, linewidth=1.3); hold on;
-title("Pitch Rate")
-xlabel("time (s)");
-ylabel("q (rad/s)");
-subplot(3,1,3);
-plot(time,aircraft_state_array(12,:),col, linewidth=1.3); hold on;
-title("Yaw Rate")
-xlabel("time (s)");
-ylabel("r (rad/s)");
-if leg ~= ""
-    legend(leg,'Position',[0.633841768165843 0.899580306092237 0.205357139451163 0.0773809503941308])
-end
-if bool==true
-    f = gcf;
-    f.WindowState = 'maximized'; 
-    pause(0.5); 
-    exportgraphics(f, "figure"+num2str(fig(4))+".png", 'Resolution', 300);
-end
-figure(fig(5));
-sgtitle("Control Inputs")
-subplot(2,2,1);
-plot(time,control_input_array(1,:), col, linewidth=1.3); hold on;
-title("Elevator")
-xlabel("time (s)");
-ylabel("\delta_e (deg)");
-subplot(2,2,2);
-plot(time,control_input_array(2,:), col, linewidth=1.3); hold on;
-title("Aileron")
-xlabel("time (s)");
-ylabel("\delta_a (deg)");
-subplot(2,2,3);
-plot(time,control_input_array(3,:), col, linewidth=1.3); hold on;
-title("Rudder")
-xlabel("time (s)");
-ylabel("\delta_r (deg)");
-subplot(2,2,4);
-plot(time,control_input_array(4,:), col, linewidth=1.3); hold on;
-title("Thrust")
-xlabel("time (s)");
-ylabel("\delta_t");
-if leg ~= ""
-    legend(leg,'Position',[0.485590278957453 0.476462444742479 0.0661458321536577 0.0379023873793621])
-end
-if bool==true
-        f = gcf;
-        f.WindowState = 'maximized'; 
-        pause(0.5); 
-        exportgraphics(f, "figure"+num2str(fig(5))+".png", 'Resolution', 300);
-end
-figure(fig(6));
-plot3(aircraft_state_array(1,:),aircraft_state_array(2,:),-aircraft_state_array(3,:), col, linewidth=1.3); hold on
-title("Complete Recorded Flight Path")
-xlabel("x pos (m)");
-ylabel("y pos (m)");
-zlabel("negative z pos (m)")
-if leg ~= ""
-    legend(leg,location="best")
-end
-if bool==true
-    f = gcf;
-    f.WindowState = 'maximized'; 
-    pause(0.5); 
-    exportgraphics(f, "figure"+num2str(fig(6))+".png", 'Resolution', 300);
-end
+    % seperating aircraft state vector and control inputs for plotting
+    x = aircraft_state_array(1,:);
+    y = aircraft_state_array(2,:);
+    z = aircraft_state_array(3,:);
+    phi = aircraft_state_array(4,:);
+    theta = aircraft_state_array(5,:);
+    psi = aircraft_state_array(6,:);
+    u = aircraft_state_array(7,:);
+    v = aircraft_state_array(8,:);
+    w = aircraft_state_array(9,:);
+    p = aircraft_state_array(10,:);
+    q = aircraft_state_array(11,:);
+    r = aircraft_state_array(12,:);
+    
+    sigma_elevator = control_input_array(1,:);
+    sigma_aileron = control_input_array(2,:);
+    sigma_rudder = control_input_array(3,:);
+    sigma_throttle = control_input_array(4,:);
+    
+    % inertial position
+    figure(fig(1));
+    sgtitle(['Inertial Position - ' casename])
+    subplot(311);
+    plot(time, x, col); hold on;
+    grid on;
+    ylabel('X-Posiiton (m)')
+    subplot(312);
+    plot(time, y, col); hold on;
+    grid on;
+    ylabel('Y-Posiiton (m)')
+    subplot(313);
+    plot(time,z,col); hold on;
+    grid on;
+    ylabel('Z-Posiiton (m)')
+    xlabel('Time (s)')
+    
+    % Euler Angles
+    figure(fig(2));
+    sgtitle(['Euler Angles - ' casename])
+    subplot(311);
+    plot(time,phi,col); hold on;
+    grid on;
+    ylabel('\phi Roll (rad)')
+    subplot(312);
+    plot(time,theta,col); hold on;
+    grid on;
+    ylabel('\theta Pitch (rad)')
+    subplot(313);
+    plot(time,psi,col); hold on;
+    grid on;
+    ylabel('\psi Yaw (rad)')
+    xlabel('Time (s)')
+    
+    % Inertial Velocity in Body Frame
+    figure(fig(3));
+    sgtitle(['Body Velocity - ' casename])
+    subplot(311);
+    plot(time,u,col); hold on;
+    grid on;
+    ylabel('u-Velocity (m/s)')
+    subplot(312);
+    plot(time,v,col); hold on;
+    grid on;
+    ylabel('v-Velocity (m/s)')
+    subplot(313);
+    plot(time,w,col); hold on;
+    grid on;
+    ylabel('w-Velocity (m/s)')
+    xlabel('Time (s)')
+    
+    % Angular Velocity
+    figure(fig(4));
+    sgtitle(['Angular Velocity - ' casename])
+    subplot(311);
+    plot(time,p,col); hold on;
+    grid on;
+    ylabel('p-Component (rad/s)')
+    subplot(312);
+    plot(time,q,col); hold on;
+    grid on;
+    ylabel('q-Component (rad/s)')
+    subplot(313);
+    plot(time,r,col); hold on;
+    grid on;
+    ylabel('r-Component (rad/s)')
+    xlabel('Time (s)')
 
+    % Control Inputs
+    figure(fig(5));
+    sgtitle(['Control Inputs - ' casename])
+    subplot(411)
+    plot(time, sigma_elevator, col); hold on;
+    grid on;
+    ylabel('sigma_elevator (deg)')
+    subplot(412)
+    plot(time, sigma_aileron, col); hold on;
+    grid on;
+    ylabel('sigma_aileron (deg)')
+    subplot(413)
+    plot(time, sigma_rudder, col); hold on;
+    grid on;
+    ylabel('sigma_rudder (deg)')
+    subplot(414)
+    plot(time, sigma_throttle, col); hold on;
+    grid on;
+    ylabel('sigma_throttle (fraction of 1)')
+    xlabel('Time (s)')
+    
+    % 3D Path
+    figure(fig(6));
+    plot3(x, y, -z, col); hold on;
+    
+    plot3(aircraft_state_array(1,1), aircraft_state_array(2,1), -aircraft_state_array(3,1), 'go');
+    
+    plot3(aircraft_state_array(1,end), aircraft_state_array(2,end), -aircraft_state_array(3,end), 'ro');
+    
+    grid on
+    xlabel('Inertial X-Position (m)')
+    ylabel('Inertial Y-Position (m)')
+    zlabel('Height [real world, flipped from inertial frame] (m)')
+    title(['Aircraft 3D Path - ' casename])
+
+    %%%%%% SAVES EVERYTHING AS A PDF
+    % letter = ['A','B','C','D','E','F'];
+    % for i = 1:6
+    %     exportgraphics(figure(fig(i)), taskname+"_"+partname+"_"+graphcasename+"_"+letter(i)+".png", 'ContentType','image', 'Resolution', 200);
+    % end
+    %%%%%%%% ONLY UNCOMMENT TO HAVE 1 MILLION PDF's
 end
