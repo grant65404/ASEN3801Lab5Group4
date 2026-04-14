7c85d007f5db88a8ecb8d40eb2b9d5b178a46aaf
Colby-Muchlinski <colby.muchlinski@colorado.edu>
2026-04-14 14:12:00 -0600

    Add files via upload

--- ElevatorChange.m
+++ ElevatorChange.m
@@ -1,4 +1,22 @@
+%Contributors: Colby Muchlinski
+%Course Number: ASEN 3801
+%FileName: ElevatorChange.m
+%Created: 4/14/2026
+
 function aircraft_surfaces_adjust = ElevatorChange(aircraft_surfaces,doublet_time,doublet_size)
+%Inputs:
+%aircraft_surfaces: the initial aircraft control surface states
+%doublet_time: the time were the doublet occurs
+%double_size: the amount of change in elevator angle due to the doublet
+
+%Outputs:
+%aircraft_surfaces_adjust: the adusted aircraft control surface states
+
+%Methodology:
+%Use an if statement to determine if before or after the doublet time, and
+%apply the double appropiately depending on that time
+
+
 
 pulse = [doublet_size;0;0;0];
 
@@ -16,7 +34,7 @@ elseif t > doublet_time && t <= 2*doublet_time
 elseif t > 2*doublet_time
 
 
- aircraft_surfaces_adjust = airfract_surfaces;
+ aircraft_surfaces_adjust = aircraft_surfaces;
 end
 
 
